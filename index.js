8ef2d6eb0db2bea25d7a46419f963dc40c2fa246
rukia <ruqayashar1@gmail.com>
2023-03-17 16:10:39 +0300

    control lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  
+  let price=20;
+   if (feet < 401){
+     return'This one is on me!';
+  }
+    else if (feet>2000 && feet<2501){
+    return'I will gladly take your thirty bucks.';
+  
+  }
+  else if(feet>=2501){
+    return'No can do.';
+  }
+  else
+  return`${price}`;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let NYC;
+ return city==="NYC"?"Ok, sounds good."
+ :"No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch(Tip){
+    case "generous":return "Thank you so much.";
+    case "not as generous":return "Thank you.";
+    default: return "Bye.";
+  }
+ 
 }
